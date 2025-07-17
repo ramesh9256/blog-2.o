5535a0bf500657136ec267d6cfe5de5e94fd5fb6
Ramesh kumar <rk9461711@gmail.com>
2025-07-17 11:43:39 +0530

    some more update

--- Frontend/src/utils/axios.js
+++ Frontend/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://blog-app-hwv2.onrender.com/api/v1',
+  baseURL: 'https://blog-app-hwv2.onrender.com/',
   withCredentials: true
 });
 
