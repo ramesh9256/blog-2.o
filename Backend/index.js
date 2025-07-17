b618470966ed172867ccd36807b9b69c9973d846
Ramesh kumar <rk9461711@gmail.com>
2025-07-17 16:16:09 +0530

    new update

--- Backend/index.js
+++ Backend/index.js
@@ -11,9 +11,25 @@ const connectDb = require('./config/db')
 const app = express();
 
 connectDb();
+// app.use(cors({
+//     origin: "https://blog-app-silk-rho.vercel.app/", // ✅ frontend origin
+//     credentials: true,
+// }));
+const allowedBase = "vercel.app";
 app.use(cors({
-    origin: "https://blog-app-silk-rho.vercel.app/", // ✅ frontend origin
-    credentials: true,
+    origin: function (origin, callback) {
+        // Allow requests with no origin (like mobile apps, curl, Postman)
+        if (!origin) return callback(null, true);
+
+        // Allow if origin includes vercel.app
+        if (origin.includes(allowedBase)) {
+            callback(null, true);
+        } else {
+            console.log("Blocked by CORS: ", origin);
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
+    credentials: true
 }));
 app.use(express.json());
 app.use(morgan());
@@ -21,7 +37,7 @@ app.use(cookieParser());
 
 app.use('/api/v1/user', userRoutes)
 app.use('/api/v1/blog', blogRoutes)
-app.use("/api", contactRoutes); 
+app.use("/api", contactRoutes);
 
 app.get('/', (req, res) => {
     res.send("hello")
